310ae62b50d40c811758bbebbf5b7580c8ad63de
JuancaJcA <avilafloresjc@gmail.com>
2023-04-21 22:24:52 -0400

    Project created

---
+++ src/main/kotlin/com/example/swaggateway/SwagGatewayApplication.kt
@@ -0,0 +1,11 @@
+package com.example.swaggateway
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SwagGatewayApplication
+
+fun main(args: Array<String>) {
+	runApplication<SwagGatewayApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/swaggateway/SwagGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.swaggateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SwagGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
